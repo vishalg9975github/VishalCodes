81ecb171e656ee9be37fd195ed1b535770280344
vishalg9975github <vishalgwaghmare31@gmail.com>
2023-04-26 13:34:15 +0530

    added two files

---
+++ VishalProject/src/com/vishal/project/StudyClass.java
@@ -0,0 +1,10 @@
+package com.vishal.project;
+
+public class StudyClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is example code");
+	}
+
+}
---
+++ VishalProject/src/com/vishal/project/StudyClass1.java
@@ -0,0 +1,10 @@
+package com.vishal.project;
+
+public class StudyClass1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is also example code...");
+	}
+
+}
